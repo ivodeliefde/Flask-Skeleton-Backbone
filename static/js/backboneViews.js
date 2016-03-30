20a0385ead030e9f2e81e78263e46f551711d2ac
ivodeliefde <ivodeliefde@gmail.com>
2016-03-30 12:45:01 +0200

    added remove marker button

--- static/js/backboneViews.js
+++ static/js/backboneViews.js
@@ -93,7 +93,7 @@
             });
         },
         highlightToggle: function(e){
-            if ($("#featureInput").attr("disabled") == 'disabled'){
+            if (window.disableHighlight == true){
                 return
             };
             if (e.target.feature.highlighted == undefined){
@@ -156,7 +156,7 @@
         }
 
     });
-
+    
     var MapView = new MapView();
 
     window.ListView = Backbone.View.extend({
@@ -168,14 +168,14 @@
           'change select#featureInputType' : 'getGeoJSON',
           // 'click button.getForm' : 'getForm1',
           'click button.getForm' : 'cleanup',
-          'click button.getGraph' : 'getGraph',
+          'click button.getSensorData' : 'getSensorData',
           'click button.removeMarker' : 'removeMarker'
         },
         form1Template: _.template($('#form1Template').html()),
         form2Template: _.template($('#form2Template').html()),
 
         initialize: function() {
-            
+            window.disableHighlight = false
             // console.log("start app");
 
             this.getForm1()
@@ -220,24 +220,25 @@
                 if (features[i] == " "){
                     // console.log("Space")
                     delete features[i];
-                }
-            }
-            features = features.join("")
-            console.log( features );
-            var featureCategory = $('#featureInputType').val();
-            if (featureCategory.indexOf("Raster") > -1){
-                featureCategory = "raster"
-            }
-            console.log(featureCategory);
-            var obsProperty = "http://dbpedia.org/resource/"+$('#obsProperty').val();
-            console.log(obsProperty);
+                };
+            };
+            window.features = features.join("");
+            console.log( window.features );
+            window.featureCategory = $('#featureInputType').val();
+            if (window.featureCategory.indexOf("Raster") > -1){
+                window.featureCategory = "raster";
+            };
+
+            console.log(window.featureCategory);
+            window.obsProperty = "http://dbpedia.org/resource/"+$('#obsProperty').val();
+            console.log(window.obsProperty);
 
             problems = []
 
             if (features.length == 0){
                 $('#featureInput').css('border','2px solid red');
                 problems.push("features");
-            }
+            };
             // if ($('#tempGranValue').val().length == 0){
             //     $('#tempGranValue').css('border','2px solid red');
             //     problems.push("temporal granularity value");
@@ -259,11 +260,11 @@
                 // $('#slider').dateRangeSlider("disable");
                 // alert("Still under construction.. Sorry!");
                 input = JSON.stringify({
-                    feature: features, 
-                    featureType: featureCategory, 
-                    // tempRange: startTime+","+endTime
-                })
-
+                    feature: window.features, 
+                    featureType: window.featureCategory, 
+                    obsProperty: window.obsProperty
+                });
+                window.disableHighlight = true;
                 $.ajax({
                   type: "POST",
                   context: this,
@@ -276,7 +277,7 @@
                 });
 
                 
-            }
+            };
 
 
 
@@ -293,6 +294,7 @@
 
         },
         getForm1: function(){
+            window.disableHighlight = false;
             $("#vis").html(this.form1Template);
 
             // $("#slider").dateRangeSlider();
@@ -310,17 +312,14 @@
 
         },
         foundSensors: function (data) {
-            data = JSON.parse(data.sensors)
-            console.log(data.type);
+            
+            window.sensors = JSON.parse(data.sensors)
+            console.log(window.sensors.type);
            
-            function onEachFeature(feature, layer) {
-                var popupContent = "<div class='row' id='popup'><table style='width:100%'><tr><td>URI</td><td><a href='"+feature.properties.sensorUri+"' target=_blank>" + feature.properties.sensorUri +"</td></tr><tr><td>Observed property</td><td><a href='" + feature.properties.observedProperty[1]  +"' target=_blank>"+feature.properties.observedProperty[1]+"</td></tr><tr><td>SOS</td><td>" + feature.properties.sos +"</td></tr></table><button class='button-primary removeMarker'>Remove</button></div>";
+            
 
-                layer.bindPopup(popupContent);
-            };
-
-            window.geojsonSensors = L.geoJson(data,{
-                onEachFeature: onEachFeature,
+            window.geojsonSensors = L.geoJson(window.sensors,{
+                onEachFeature: this.onEachMarker,
                 pointToLayer: function (feature, latlng) {
                 return L.circleMarker(latlng, {
                     radius: 8,
@@ -343,43 +342,105 @@
             console.log(f);
         },
 
-        getGraph: function(){
-            var temporalGranularity = $('#tempGranValue').val() +" "+ $('#tempGranUnit').val();
-            console.log(temporalGranularity);
-            var tempAggType = $('#temporalAggType').val();
-            console.log("temporal aggregation: "+tempAggType);
-            var spatialAggType = $('#spatialAggType').val();
-            console.log("spatial aggregation: "+spatialAggType);
-            var startTime = new Date($('#slider').dateRangeSlider("values").min.setUTCHours(0)).toISOString();
-            console.log(startTime);
-            var endTime = new Date($('#slider').dateRangeSlider("values").max.setUTCHours(0)).toISOString();
-            console.log(endTime);
+        getSensorData: function(){
+            console.log("Get sensor data");
+            window.temporalGranularity = $('#tempGranValue').val() +" "+ $('#tempGranUnit').val();
+            console.log(window.temporalGranularity);
+            window.tempAggType = $('#temporalAggType').val();
+            console.log("temporal aggregation: "+window.tempAggType);
+            window.spatialAggType = $('#spatialAggType').val();
+            console.log("spatial aggregation: "+window.spatialAggType);
+            window.startTime = new Date($('#slider').dateRangeSlider("values").min.setUTCHours(0)).toISOString();
+            console.log(window.startTime);
+            window.endTime = new Date($('#slider').dateRangeSlider("values").max.setUTCHours(0)).toISOString();
+            console.log(window.endTime);
 
             MapView.loadGeoJSON(undefined);
 
-            $('#tempGranValue').attr("disabled", "disabled"); 
-            $('#tempGranUnit').attr("disabled", "disabled");
-            $('#spatialAggType').attr("disabled", "disabled"); 
-            $('#temporalAggType').attr("disabled", "disabled"); 
- 
-            $('#slider').dateRangeSlider("disable");
+            problems = []
 
-            var input = JSON.stringify({
-                    feature: features, 
-                    featureType: featureCategory, 
-                    // tempRange: startTime+","+endTime
-                })
+            if ($('#tempGranValue').val().length == 0){
+                $('#tempGranValue').css('border','2px solid red');
+                problems.push("temporal granularity value");
+            }
+            console.log(problems);
+            if (problems.length > 0){
+                errorText = '<h5>Please select '+problems.join(' & ')+'</h5>';
+                $('#error').html(errorText);
+            } else {
+                console.log("send data");
+                $('#error').html('');
+                $('.getSensorData').html('Calculating...');
+                $('button.getSensorData').prop('disabled', true);
+                $('#tempGranValue').attr("disabled", "disabled"); 
+                $('#tempGranUnit').attr("disabled", "disabled"); 
+                $('#spatialAggType').attr("disabled", "disabled"); 
+                $('#temporalAggType').attr("disabled", "disabled"); 
+                $('#slider').dateRangeSlider("disable");
 
-            $.ajax({
-                  type: "POST",
-                  context: this,
-                  contentType: "application/json; charset=utf-8",
-                  url: "/getSensorData",
-                  data: input,
-                  success: this.foundSensors,
-                  error: this.errorLog,
-                  dataType: "json"
-                });
+                var input = JSON.stringify({
+                        sensors: window.sensors,
+                        feature: window.features, 
+                        featureType: window.featureCategory, 
+                        obsProperty: window.obsProperty,
+                        tempGran: window.temporalGranularity,
+                        startTime: window.startTime,
+                        endTime: window.endTime,
+                        tempAggType: window.tempAggType,
+                        spatialAggType: window.spatialAggType
+                    });
+
+                $.ajax({
+                      type: "POST",
+                      context: this,
+                      contentType: "application/json; charset=utf-8",
+                      url: "/getSensorData",
+                      data: input,
+                      success: this.createGraph,
+                      error: this.errorLog,
+                      dataType: "json"
+                    });
+            }
+            
+        },
+        removeMarker: function(e){
+            console.log("remove marker");
+            window.geojsonSensors.clearLayers();
+            var uri = $(e.currentTarget).attr('name')
+            for (var feature in window.sensors.features) {    
+                // console.log(window.sensors.features[feature].properties);
+                if (window.sensors.features[feature].properties != undefined) {
+                    if (uri == window.sensors.features[feature].properties.sensorUri){
+                        // console.log("delete "+window.sensors.features[feature].properties.sensorUri);
+                        // console.log(window.sensors.features[feature]);
+                        // console.log(window.sensors.features.length);
+                        delete window.sensors.features.splice( feature, 1);
+                        // console.log(window.sensors.features.length);
+                    }
+                }
+               
+                
+            }
+            // console.log(window.sensors);
+            window.geojsonSensors = L.geoJson(window.sensors,{
+                onEachFeature: this.onEachMarker,
+                pointToLayer: function (feature, latlng) {
+                return L.circleMarker(latlng, {
+                    radius: 8,
+                    fillColor: "#ff7800",
+                    color: "#000",
+                    weight: 1,
+                    opacity: 1,
+                    fillOpacity: 0.8
+                    });
+                }
+            }).addTo(window.map);
+        },
+        createGraph: function(data){
+            console.log("create graph");
+            console.log(data);
+            var url = data.url;
+            console.log(url);
 
             vlSpec = {
               "description": "A trellis bar chart showing the US population distribution of age groups and gender in 2000.",
@@ -414,12 +475,15 @@
             vg.embed("#vis", embedSpec, function(error, result) {
               // Callback receiving the View instance and parsed Vega spec
               // result.view is the View, which resides under the '#vis' element
+              console.log(error,result);
             });
-            console.log("add to #vis")
-            $('#vis').append("<button class='button-primary getForm'>Back to form</button>")
+            console.log("add to #vis");
+            $('#vis').append("<button class='button-primary getForm'>Back to form</button>");
         },
-        removeMarker: function(){
-            console.log("remove marker");
+        onEachMarker: function(feature, layer) {
+            var popupContent = "<div class='row' id='popup'><table style='width:100%'><tr><th colspan='2'><center><h1>Sensor</h1></center></th></tr><tr><td>URI</td><td><a href='"+feature.properties.sensorUri+"' target=_blank>" + feature.properties.sensorUri +"</td></tr><tr><td>Observed property</td><td><a href='" + feature.properties.observedProperty[1]  +"' target=_blank>"+feature.properties.observedProperty[1]+"</td></tr><tr><td>SOS</td><td><a href='" + feature.properties.sos  +"service=SOS&version=2.0.0&request=GetCapabilities' target=_blank>" + feature.properties.sos +"</a></td></tr></table><button class='removeMarker' name='"+feature.properties.sensorUri+"'>Remove</button></div>";
+
+            layer.bindPopup(popupContent);
         }
 
 
